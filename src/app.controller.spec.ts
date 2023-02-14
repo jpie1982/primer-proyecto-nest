5910fa8b705e339a818a8c0171770070d0ee677d
Juan Pablo <jpie1982@gmail.com>
2023-02-14 00:53:07 -0300

    Add files via upload

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Este es mi primera aplicación en Nest"', () => {
+      expect(appController.getHello()).toBe('Este es mi primera aplicación en Nest');
+    });
+  });
+});
